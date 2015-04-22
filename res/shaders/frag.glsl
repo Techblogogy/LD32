8ae7f41f37390a76d197f3e42ff62f3011524002
Techblogogy <fedorbobylev@gmail.com>
2015-04-22 14:19:17 +0300

    Chrome Mint Issue

--- res/shaders/frag.glsl
+++ res/shaders/frag.glsl
@@ -1,8 +1,8 @@
 precision highp float; 
 precision highp sampler2D; 
 
-// varying vec2 vTexPs; 
-varying vec2 vLmpPs;
+varying vec2 vTexPs; 
+// varying vec2 vLmpPs;
 
 uniform sampler2D tex; 
 uniform vec2 texOff;
